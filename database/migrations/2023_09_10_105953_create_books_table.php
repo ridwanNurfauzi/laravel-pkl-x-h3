42412d6295682fc7d38ecf5b4b628b83b3d73b67
ridwanNurfauzi <mrnhp123@gmail.com>
2023-09-10 19:25:01 +0700

    first commit

---
+++ database/migrations/2023_09_10_105953_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('author_id')->unsigned();
+            $table->integer('amount')->unsigned();
+            $table->string('cover')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
